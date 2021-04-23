858d09a35ff690090689b4454614246bd7c62c0a
Anindya-2000 <sikdarani01@gmail.com>
2021-04-23 11:05:58 +0530

    TestPrep Project Created

---
+++ TestPrep/TestCreator/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TestcreatorConfig(AppConfig):
+    name = 'TestCreator'
---
+++ TestPrep/TestSimulator/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TestsimulatorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'TestSimulator'
